0ea119c9815c0d3102f64fac99fd0abf262badbc
Velyo Ivanov <velio.ivanov@gmail.com>
2016-12-04 23:05:40 +0200

    add indent

--- src/Velyo.AspNet.Markdown/Markdown.cs
+++ src/Velyo.AspNet.Markdown/Markdown.cs
@@ -1,17 +1,61 @@
 ﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Markdig;
 
 namespace Velyo.AspNet
 {
     [ToolboxData("<{0}:Markdown runat=\"server\"></{0}:Markdown>")]
+    [ParseChildren(true, nameof(Content))]
     public class Markdown : WebControl
     {
         [Browsable(false)]
+        [Bindable(true)]
         [Category("Markdown")]
-        [DefaultValue(null)]
+        [DefaultValue("")]
         [Description("Markdown content")]
-        [PersistenceMode(PersistenceMode.InnerProperty)]
-        public virtual ITemplate Content { get; set; }
+        [PersistenceMode(PersistenceMode.EncodedInnerDefaultProperty)]
+        public virtual string Content { get; set; }
+
+        public int Indent { get; set; }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            if (!string.IsNullOrWhiteSpace(Content))
+            {
+                var content = Content;//.Trim();
+
+                if (Indent > 0)
+                {
+                    var buffer = new StringBuilder();
+
+                    using (var reader = new StringReader(content))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            if (line.Length > Indent)
+                            {
+                                buffer.AppendLine(line.Substring(Indent));
+                            }
+                            else
+                            {
+                                buffer.AppendLine(line);
+                            }
+                        }
+                    }
+
+                    content = buffer.ToString();
+                }
+
+                // TODO cache this
+                var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                var result = Markdig.Markdown.ToHtml(content, pipeline);
+
+                writer.Write(result);
+            }
+        }
     }
 }
--- test/Velyo.AspNet.Markdown.Samples/Sample.aspx
+++ test/Velyo.AspNet.Markdown.Samples/Sample.aspx
@@ -1,7 +1,25 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Sample.aspx.cs" Inherits="Velyo.AspNet.Samples.Sample" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <velyo:Markdown runat="server">
-        
-    </velyo:Markdown>
+    <velyo:MarkdownControl runat="server">
+# ASP.NET Markdown
+
+ASP.NET Markdown control to embed and render markdown content in your ASP.NET application.
+
+## Features
+
+
+## Contribute
+
+Check out the [contribution guidelines](https://github.com/velyo/aspnet-markdown/blob/master/CONTRIBUTING.md) if you want to contribute to this project.
+
+## License
+
+[MIT](https://github.com/velyo/aspnet-markdown/blob/master/LICENSE)
+
+```cs
+public class Test {
+}
+```
+    </velyo:MarkdownControl>
 </asp:Content>
